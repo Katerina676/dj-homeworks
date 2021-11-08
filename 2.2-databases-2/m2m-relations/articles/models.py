63ab3a79c8c1149a6773003457e9433f02f3045b
Ekaterina Baeva <ameli.pullen@mail.ru>
2021-11-08 17:22:27 +0300

    commit

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
 
+class Tags(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+
 class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение', )
+    image = models.ImageField(null=True,
+                              blank=True,
+                              verbose_name='Изображение'
+                              )
+    tags = models.ManyToManyField(Tags, through='Scopes')
 
     class Meta:
         verbose_name = 'Статья'
@@ -13,3 +28,20 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Scopes(models.Model):
+    tag = models.ForeignKey(Tags,
+                            on_delete=models.CASCADE,
+                            verbose_name='Раздел'
+                            )
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
+    article = models.ForeignKey(Article,
+                                on_delete=models.CASCADE,
+                                related_name='scopes'
+                                )
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики Статьи'
+        ordering = ['-is_main', 'tag__name']
